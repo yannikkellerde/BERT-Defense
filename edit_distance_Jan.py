35173fb8b3076fe1de247a7a481861bfefa24730
JanMackensen <jan.mackensen@web.de>
2020-06-07 16:39:56 +0200

    simple levenshtein distance

---
+++ edit_distance_Jan.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def levenshteinDistance(target, source):
+    n = len(target)
+    m = len(source)
+    distance = np.zeros((n+1, m+1))
+    for i in range(1,n+1):
+        distance[i][0] = i
+    for j in range(1,m+1):
+        distance[0][j] = j
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if target[i-1]==source[j-1]:
+                distance[i][j] = distance[i-1][j-1]
+            else:
+                distance[i][j] = min((distance[i-1][j] + in_cost(target[i-1]),       # insertion von target_i in source
+                                      distance[i-1][j-1] + sub_cost(target[i-1], source[j-1]),     # substituition von target in source
+                                      distance[i][j-1] + del_cost(source[j-1])))      # delition  source_j
+    return distance[n][m]
+
+def in_cost(in_char):
+    return 1
+
+def sub_cost(char1, char2):
+    return 1
+
+def del_cost(del_char):
+    return 1
+
+print(levenshteinDistance("Hand","H/a/n/d/b/a/l/l"))
