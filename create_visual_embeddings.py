a96c2fcfe554801befddd443c83efe93c9dabe88
yannikkellerde <yannik@kelnet.de>
2020-06-08 18:51:05 +0200

    Hab die visual embeddings mal durch ihre norm geteilt, wodurch die cosine similarity jetzt äquivalent zum einfachen dot produkt ist welches um einiges schneller sein sollte.

--- create_visual_embeddings.py
+++ create_visual_embeddings.py
@@ -14,11 +14,13 @@ for i in trange(32,30000,1):
     d = ImageDraw.Draw(txt)
     d.text((0,0), chr(i), font=fnt, fill=(0,))
     vec_dict[i] = np.array(txt).flatten().astype(int)
-all_vecs = np.array(list(vec_dict.values()))
-mean = np.mean(all_vecs)
-std = np.std(all_vecs)
-print(mean,std)
-for key in tqdm(vec_dict):
-    vec_dict[key] = (vec_dict[key]-mean)/std
+for i in range(20):
+    for key in tqdm(vec_dict):
+        vec_dict[key] = vec_dict[key]/np.linalg.norm(vec_dict[key])
+    all_vecs = np.array(list(vec_dict.values()))
+    mean = np.mean(all_vecs)
+    for key in tqdm(vec_dict):
+        vec_dict[key] = (vec_dict[key]-mean)
+    print(mean,np.linalg.norm(vec_dict[ord("O")]))
 with open("visual_embeddings.pkl","wb") as f:
     pkl.dump(vec_dict,f)
