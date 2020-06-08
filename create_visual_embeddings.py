83ec39198d998897e089792b4b616530e4f3bfb3
yannikkellerde <yannik@kelnet.de>
2020-06-08 12:21:44 +0200

    Norm embedding vectors

--- create_visual_embeddings.py
+++ create_visual_embeddings.py
@@ -14,6 +14,11 @@ for i in trange(32,30000,1):
     d = ImageDraw.Draw(txt)
     d.text((0,0), chr(i), font=fnt, fill=(0,))
     vec_dict[i] = np.array(txt).flatten().astype(int)
-
+all_vecs = np.array(list(vec_dict.values()))
+mean = np.mean(all_vecs)
+std = np.std(all_vecs)
+print(mean,std)
+for key in tqdm(vec_dict):
+    vec_dict[key] = (vec_dict[key]-mean)/std
 with open("visual_embeddings.pkl","wb") as f:
     pkl.dump(vec_dict,f)
