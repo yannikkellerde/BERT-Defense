593d2539c93a96cc309864382bbab68684d04675
JanMackensen <jan.mackensen@web.de>
2020-06-14 18:02:51 +0200

    embedding

--- __main__.py
+++ __main__.py
@@ -20,26 +20,33 @@ logger.addHandler(ch)
 import numpy as np
 from bert_posterior import bert_posterior, format_dict
 from edit_distance import get_word_dic_distance
-from util import load_dictionary,load_pickle,load_and_preprocess_dataset
+from util import load_dictionary,load_pickle,load_and_preprocess_dataset,cosine_similarity
 from letter_stuff import sentence_ends
-from util import multiprocess_word_distances
+from multiprocess_tasks import multiprocess_word_distances
+from sentence_Embeddings import load_vectors, sentence_embedding_only_best_word,init_model_roberta, sentence_average_from_word_embeddings
 import time
+
 
 if __name__ == '__main__':
     logger.info("loading dictionary, word embedding and dataset")
     letter_dic = load_dictionary("DATA/dictionaries/bert_letter_begin.txt")
     number_dic = load_dictionary("DATA/dictionaries/bert_number_begin.txt")
     punc_dic = load_dictionary("DATA/dictionaries/bert_punctuations.txt")
+    word_vecs = load_vectors("DATA/embeddings/wiki-news-300d-1M-subword.txt")
+    # word_vecs = load_vectors("DATA/embeddings/wiki-news-300d-1M-subword.vec")
     dictionary = letter_dic+number_dic+punc_dic
     word_embedding = load_pickle("visual_embeddings.pkl")
     bert_dict = format_dict(dictionary)
     dataset = load_and_preprocess_dataset("DATA/test-scoreboard-dataset.txt")
-    in_data = dataset[0:1]
+    in_data = dataset[0:5]
     logger.info(in_data)
+    logger.info("Init Sentence Embedding Model")
+    model = init_model_roberta()
     logger.info("calculating distances")
     start = time.perf_counter()
     priors = multiprocess_word_distances(in_data,dictionary,word_embedding)
     posterior_sentences = [[[] for _sentence in line] for line in priors]
+    embedded_sentences =  [[[] for _sentence in line] for line in priors]
     for l,line in enumerate(priors):
         for s,sentence_prior in enumerate(line):
             prior_sentence = [list(sorted([(y[1],y[0]) for y in x],reverse=True))[0][1] for x in sentence_prior]
@@ -50,11 +57,18 @@ if __name__ == '__main__':
             bert_prior = np.array([[x[1] for x in p] for p in sentence_prior])
             logger.info("calculating posterior")
             posterior = bert_posterior(bert_prior,bert_dict,10)
+            sent_emb = sentence_embedding_only_best_word(model, posterior, dictionary)
+            sent_emb2 = sentence_average_from_word_embeddings(posterior, dictionary, word_vecs)
+            embedded_sentences[l][s] = sent_emb
             out_sentence = [dictionary[np.argmax(p)] for p in posterior]
             logger.info("Posterior sentence"+str(out_sentence))
             posterior_sentences[l][s] = out_sentence
-
+    sim_scores = []
+    for line in embedded_sentences:
+        sim_scores.append(cosine_similarity(line[0], line[1]))
     logger.info(f"time taken: {time.perf_counter()-start}")
 
     logger.info("full posterior:")
     logger.info(posterior_sentences)
+    logger.info("cosine_similarity")
+    logger.info(sim_scores)
---
+++ sentence_Embeddings.py
@@ -0,0 +1,50 @@
+import numpy as np
+import io
+from sentence_transformers import SentenceTransformer
+
+
+def init_model_roberta():
+    return SentenceTransformer("roberta-large-nli-stsb-mean-tokens")
+
+
+def sentence_embedding_only_best_word(model, posterior, dic):
+    sentences = []
+    out_sentence = [dic[np.argmax(p)] for p in posterior]
+    sentence = ""
+    for word in out_sentence:
+        sentence = sentence + word + " "
+    sentences.append(sentence)
+    sentence_embeddings = model.encode(sentences)
+    return sentence_embeddings[0]
+
+
+def sentence_average_from_word_embeddings(posterior, dic, embeddings):
+    sentence_embedding =[]
+    word_vecs = []
+    for word_distribution in posterior:
+        word_vec = get_word_vec_from_distribution(word_distribution, dic, embeddings)
+        word_vecs.append(word_vec)
+    sentence_embedding.append(word_vecs.average(np.array(word_vecs), axis=0))
+    return sentence_embedding
+
+
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = np.array(list(map(float, tokens[1:])))
+    return data
+
+
+def get_word_vec_from_distribution(word_distribution, dic, embeddings):
+    word_vecs = []
+    weigths = []
+    for i in range(len(word_distribution)):
+        if word_distribution[i] !=0:
+            word_vecs.append(embeddings[dic[i]])
+            weights.append(word_distribution[i])
+    word_vecs = np.array(word_vecs)
+    average_word = np.average(word_vecs, axis=0, weights=weigths)
+    return average_word
