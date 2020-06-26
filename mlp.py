87db1cbb0ef873a29852bd775cd21deec178b885
JanMackensen55 <jan.mackensen@web.de>
2020-06-26 11:06:34 +0200

    mlp random testing hope it woks cant test it

--- mlp.py
+++ mlp.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import pickle as pkl
 from util import read_labeled_data
+from tqdm import trange
+import random
 from sentence_Embeddings import init_model_roberta, simple_sentence_embedder
 
 
@@ -46,37 +48,62 @@ def load_all_data():
     return train_score, train_1, train_2, dev_score, dev_1, dev_2, test_1, test_2
 
 
-def create_model(layerss, dropouts, activation_func):
+def create_model(layerss, dropout, activation_func):
     input1 = tf.keras.layers.Input(shape=(1024,))
     input2 = tf.keras.layers.Input(shape=(1024,))
     conc = tf.keras.layers.Concatenate()([input1, input2])
-    drop = tf.keras.layers.Dropout(dropouts[0])(conc)
+    drop = tf.keras.layers.Dropout(dropout)(conc)
     layer = tf.keras.layers.Dense(layerss[0], activation=activation_func)(drop)
-    for l, d in zip(layerss[1:],dropouts[1:-1]):
-        drop = tf.keras.layers.Dropout(d)(layer)
+    for l in layerss[1:]:
+        drop = tf.keras.layers.Dropout(dropout)(layer)
         layer = tf.keras.layers.Dense(l, activation=activation_func)(drop)
-    drop = tf.keras.layers.Dropout(dropouts[-1])(layer)
+    drop = tf.keras.layers.Dropout(dropout)(layer)
     out = tf.keras.layers.Dense(1, activation="sigmoid")(drop)
     model = tf.keras.Model((input1, input2), out)
     model.compile(loss="mse", optimizer=tf.keras.optimizers.Adam(), metrics=["mse"])
     return model
 
 
-if __name__ == '__main__':
-    layers = [1024]
-    dropout  = [0.3, 0.3]
-    batch_size = 256
-    epochs = 200
-    activation_func = "relu"
-    model = create_model(layers, dropout, activation_func)
+def random_model_testing(n, batch_size=256):
     train_score, train_1, train_2, dev_score, dev_1, dev_2, test_1, test_2 = load_all_data()
-    checkpointer = tf.keras.callbacks.ModelCheckpoint("mlp_model/best.model", save_best_only=True, 
-                                                      verbose=1,save_weights_only=True,
-                                                      monitor='val_loss')
-    model.fit((train_1, train_2), train_score, batch_size=batch_size, epochs=epochs,
-              validation_data = ((dev_1, dev_2), dev_score), callbacks=[checkpointer])
-    model.load_weights("mlp_model/best.model")
-    pred = model.predict((test_1, test_2))
-    with open("mlp_model/scores.txt","w") as f:
-        f.write("\n".join([str(x[0]) for x in pred]))
+    for i in trange(n):
+        num_layers = random.randint(1, 25)
+        dropout = random.uniform(0.05, 0.6)
+        layers = []
+        for i in range(num_layers):
+            layers.append(random.randint(50,2000))
+        model = create_model(layers, dropout, 'relu')
+        filepath = "model_" + str(layers) + str(dropout) + ".h5" 
+        callbacks = [tf.keras.callbacks.EarlyStopping(patience=10),
+                     tf.keras.callbacks.ModelCheckpoint(filepath=filepath)]
+        model.fit((train_1, train_2), train_score, batch_size=batch_size, epochs=10000,
+                  validation_data = ((dev_1, dev_2), dev_score), callbacks=[callbacks],
+                  verbose=1)
+        if i%5 == 0:
+            model_clean_up(dev_1, dev_2)
+
+
+def model_clean_up(dev_1, dev_2):
+    all_models = os.listdir("mlp_model")
+    best_model = None
+    best_file = None
+    for model in all_models:
+        filepath = "mlp_model/" + model
+        m = tf.keras.models.load_model(filepath)
+        challenger = m.model.evaluate(test_x, test_y, verbose=0)[1]
+        if not(best_model is None):
+            if best_model < challenger:
+                os.remove(best_file)
+                best_model = challenger
+                best_file = filepath
+            else:
+                os.remove(filepath)
+        else:
+            best_model = challenger
+            best_file = filepath
+
+
+if __name__ == '__main__':
+    random_model_testing(10)
+    
     
