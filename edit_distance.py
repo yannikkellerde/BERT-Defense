f579449e46061d3532b27b8342959d7e804a1856
JanMackensen55 <jan.mackensen@stud.tu-darmstadt.de>
2020-08-20 19:42:35 +0200

    change edit distance plus some tests

--- edit_distance.py
+++ edit_distance.py
@@ -4,11 +4,27 @@ from tqdm import tqdm,trange
 from operator import itemgetter
 from letter_stuff import trenner_punctuations,vocals, annoying_boys
 
-
-def levenshteinDistance(target, source, word_embedding,char_app=None,vowls_in=None,cheap_deletions=True):
-    if char_app is None:
+"""
+cheap_actions = an array d with a true or false statment for each action
+                          d[0] == True: activate cheap insertion
+                          d[1] == True: activate cheap substitution (need word_embedding)
+                          d[2] == True: activate cheap delition
+                          d[3] == True: activate cheap transposition
+                          shortcut: True activates all and False deactivated all
+"""
+def levenshteinDistance(target, source, cheap_actions=False , word_embedding=None, char_app=None,vowls_in=None,cheap_deletions=True):
+    if not(cheap_actions is list):
+        if cheap_actions:
+            cheap_actions = np.ones(4, dtype=bool)
+        else:
+            cheap_actions = np.zeros(4, dtype=bool)
+    if cheap_actions[1] and word_embedding is None:
+        raise AttributeError("need word_embedding")
+    if cheap_actions[3]:
+        anagramm = anagramm_finder(source, target)
+    if char_app is None and cheap_actions[2]:
         char_app = char_apparence(source)
-    if vowls_in is None:
+    if vowls_in is None and cheap_actions[0]:
         vowls_in = vowl_checker(source)
     n = len(target)
     m = len(source)
@@ -17,19 +33,21 @@ def levenshteinDistance(target, source, word_embedding,char_app=None,vowls_in=No
     distance = np.zeros((n+1, m+1))
     i = 0
     for num in range(1, n+1):
-        i += in_cost(target[num-1],vowls_in)
+        i += in_cost(target[num-1],vowls_in) if cheap_actions[0] else 1
         distance[num][0] = i
     j = 0
     for num in range(1, m+1):
-        j += del_cost(source[num-1],char_app,False,freelo_amount)
+        j += del_cost(source[num-1],char_app,False,freelo_amount) if cheap_actions[2] else 1
         distance[0][num] = j
     for i in range(1, n+1):
         for j in range(1, m+1):
-            possibilities = [distance[i-1][j] + in_cost(target[i-1], vowls_in),# insertion von target_i in source
-                                distance[i-1][j-1] + sub_cost(target[i-1], source[j-1], word_embedding),# substituition von target in source
-                                distance[i][j-1] + del_cost(source[j-1], char_app,i==n and cheap_deletions,freelo_amount)]# delition  source_j
+            possibilities = [distance[i-1][j] + (in_cost(target[i-1], vowls_in) if cheap_actions[0] else 1),# insertion von target_i in source
+                                distance[i-1][j-1] + (sub_cost(target[i-1], source[j-1], word_embedding) if cheap_actions[1] else 1),# substituition von target in source
+                                distance[i][j-1] + (del_cost(source[j-1], char_app,i==n and cheap_deletions,freelo_amount) if cheap_actions[2] else 1)]# delition  source_j
             if target[i-1] == source[j-1]:
                 possibilities.append(distance[i-1][j-1])
+            if i > 2 and j > 2 and target[i-1] == source[j-2] and target[i-2] == source[j-1]:
+                possibilities.append(distance[i-2][j-2] + (trans_cost(anagramm) if cheap_actions[3] else 1))
             choice = util.fast_argmin(possibilities)
             if i==n:
                 if choice==2:
@@ -58,6 +76,22 @@ def del_cost(del_char, table, freelo,freelo_amount, scaler=0.75):
     return min(freelo_amount,scal_cost) if freelo else scal_cost
 
 
+def trans_cost(anagramm):
+    if anagramm:
+        return 0.3
+    else:
+        return 1
+
+
+def anagramm_finder(source, target):
+    if not(len(source) == len(target)):
+        return False
+    for char in source:
+        if not(char in target):
+            return False
+    return True
+
+
 def vowl_checker(word):
     vowls = ["A", "a", "E", "e", "I", "i", "O", "o", "u", "U" ]
     for char in word:
@@ -79,7 +113,7 @@ def char_apparence(word):
     return table
 
 
-def get_word_dic_distance(word, dic, word_embedding, sort=True, progress=True, orig_word=None, cheap_deletions=True):
+def get_word_dic_distance(word, dic, word_embedding, cheap_actions=False, sort=True, progress=True, orig_word=None, cheap_deletions=True):
     if orig_word is None:
         orig_word = word
     char_app = char_apparence(orig_word)
@@ -89,7 +123,10 @@ def get_word_dic_distance(word, dic, word_embedding, sort=True, progress=True, o
     else:
         distance = []
         for sample_word in (tqdm(dic) if progress else dic):
-            distance.append((sample_word, *levenshteinDistance(sample_word, word, word_embedding,char_app,vowls_in,cheap_deletions)))
+            distance.append((sample_word, *levenshteinDistance(sample_word, word,cheap_actions=cheap_actions,
+                                                               word_embedding=word_embedding,
+                                                               char_app=char_app,vowls_in=vowls_in,
+                                                               cheap_deletions=cheap_deletions)))
     if sort:
         distance.sort(key=itemgetter(1))
     words, values, dels = zip(*distance)
@@ -106,6 +143,7 @@ if __name__ == '__main__':
     number_begin = util.load_dictionary("DATA/dictionaries/bert_number_begin.txt")
     dic = letter_begin+number_begin
     word_embedding = util.load_pickle("visual_embeddings.pkl")
-    distance = get_word_dic_distance("ɋ˯İՅ", dic, word_embedding, cheap_deletions=False)
+    print(levenshteinDistance("eco", "eco" , cheap_actions=True, word_embedding=word_embedding))
+    distance = get_word_dic_distance("lohle", dic, word_embedding,cheap_actions=True, cheap_deletions=False)
     for i in range(40):
         print(distance[i])
---
+++ test_prior_behavior_against_suffle.py
@@ -0,0 +1,92 @@
+import numpy as np
+import util
+from tqdm import tqdm,trange
+from edit_distance import get_word_dic_distance
+# https://github.com/yannikbenz/robustness-of-deep-learning-models-to-low-level-adversarial-attacks/blob/master/code/attacks/simple_attacks.py
+def swap(word: str, inner: bool, seed=None):
+    """Shuffles the chars in each word. If inner is set the first and last letters position remain untouched.
+    >>> swap("hello world", True, 56)
+    hlelo wlord
+    >>> swap("hello word", False, 42)
+    eolhl odrwl
+    :param word:
+    :param seed: seed
+    :param inner: if set, only the inner part of the word will be swapped
+    :return: swapped text
+    """
+
+    def __shuffle_string__(_word: str, _seed=seed):
+        """
+        shuffles the given string if a seed is given it shuffles in respect to the given seed.
+        hello world -> elloh roldw
+        :param _seed: seed
+        :param _word: string (word) to shuffle
+        :return: shuffled string
+        """
+        chars = list(_word)
+        if _seed is not None:
+            np.random.seed(_seed)
+        np.random.shuffle(chars)
+        return ''.join(chars)
+
+    if len(word) < 3 or inner and len(word) < 4:
+        return word
+    perturbed = word
+    tries = 0
+    while perturbed == word:
+        tries += 1  # we can get a deadlock if the word is e.g. maas
+        if tries > 10:
+            break
+        if inner:
+            first, mid, last = word[0], word[1:-1], word[-1]
+            perturbed = first + __shuffle_string__(mid) + last
+        else:
+            perturbed = __shuffle_string__(word)
+    return perturbed
+
+def append_each_word_suffled_to_list(sentence, suffled_words, inner=False):
+    new_words = sentence.split()
+    for word in new_words:
+        suffled_words.append((word, swap(word, inner=inner)))
+    return suffled_words
+
+
+def test_prior(cheap_actions, inner):
+    letter_begin = util.load_dictionary("DATA/dictionaries/bert_letter_begin.txt")
+    number_begin = util.load_dictionary("DATA/dictionaries/bert_number_begin.txt")
+    dic = letter_begin+number_begin
+    word_embedding = util.load_pickle("visual_embeddings.pkl")
+    with open("phonetic_attacks/sts-b-sentences.txt","r") as f:
+            sentences = f.read().splitlines()
+    suffled_words = [] 
+    for sentence in sentences:
+        sentence = sentence.lower()
+        suffled_words = append_each_word_suffled_to_list(sentence, suffled_words, inner=inner)
+    index_list = []
+    prob_list = [] 
+    for word_pair in tqdm(suffled_words):
+        distance = get_word_dic_distance(word_pair[1], dic, word_embedding, cheap_actions=cheap_actions, progress=False, cheap_deletions=False)
+        element = list(filter(lambda x: x[0]==word_pair[0], distance))
+        if element:
+            index = distance.index(element[0])
+            index_list.append(index+1)
+            prob_list.append(element[0][1])
+    print("Mean Position: ", np.mean(np.array(index_list)))
+    print("Mean Probability: ", np.mean(np.array(prob_list)))
+
+if __name__ == '__main__':
+    print("--------------------------------------------------------------")
+    print("Test Prior: cheap_actions=True, inner=True")
+    test_prior(True, True)
+    print("--------------------------------------------------------------")
+    print("Test Prior: cheap_actions=True, inner=False")
+    test_prior(True, False)
+    print("--------------------------------------------------------------")
+    print("Test Prior: cheap_actions=False, inner=True")
+    test_prior(False, True)
+    print("--------------------------------------------------------------")
+    print("Test Prior: cheap_actions=False, inner=False")
+    test_prior(False, False)
+    print("--------------------------------------------------------------")
+    
+        
