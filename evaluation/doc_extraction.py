61cbe352f8dc07d0a82e8ad8c9b7a60e3740cf45
JanMackensen55 <jan.mackensen@stud.tu-darmstadt.de>
2020-12-11 12:41:11 +0100

    experiment_data

--- evaluation/doc_extraction.py
+++ evaluation/doc_extraction.py
@@ -5,28 +5,43 @@ sys.path.append("../bayesian_shielding")
 from util.utility import read_labeled_data
 
 
-def create_human_test_data(ground_truth, attack_path, attacked_documents, sentence_per_doc ,num_per_sent):
+def create_human_test_data(ground_truth, attack_path, attacked_documents, sentence_per_attacker ,num_per_sent, num_cond):
     test_data = []
     _, tfirst_sentences, tsecond_sentences = read_labeled_data(ground_truth)
     correct_sentences = tfirst_sentences + tsecond_sentences
-    for path, sent_num in zip(attacked_documents, sentence_per_doc):
+    samples = random.sample(range(len(correct_sentences)), sentence_per_attacker)
+    for sample in samples:
+        for _ in range(num_per_sent):
+            test_data.append((correct_sentences[sample], correct_sentences[sample], "correct.txt"))
+    for path in attacked_documents:
         complete_path = os.path.join("attacked_documents",path)
         _, first_sentences, second_sentences = read_labeled_data(complete_path)
         attacked_sentences = first_sentences + second_sentences
-        samples = random.sample(range(len(attacked_sentences)), sent_num)
+        samples = random.sample(range(len(attacked_sentences)), sentence_per_attacker)
         for sample in samples:
             for _ in range(num_per_sent):
                 test_data.append((attacked_sentences[sample], correct_sentences[sample], path))
     random.shuffle(test_data)
-    stim, truth, catagory = zip(*test_data)
-    data = {"stimulus":stim, "Truth":truth, "Class":catagory}
-    df = pd.DataFrame(data=data)
-    df.to_csv("experiment_data", index=False)
-
-
-
-
-
+    cond_list = []
+    cond_list = [[] for i in range(num_cond)]
+    for i in range(len(test_data)):
+        cond_list = list(sorted(cond_list, key=lambda x: len(x), reverse=False))
+        for cond in range(len(cond_list)):
+            if not(cond_list[cond]):
+                cond_list[cond].append(test_data[i])
+                break
+            else:
+               stim, truth, catagory = zip(*cond_list[cond])
+               if not(test_data[i][1] in truth):
+                   cond_list[cond].append(test_data[i])
+                   break
+            if cond == len(cond_list)-1:
+                print("Warning Element not added")
+    for condition, i in zip(cond_list, range(len(cond_list))):
+        stim, truth, catagory = zip(*condition)
+        data = {"stimulus":stim, "Truth":truth, "Class":catagory}
+        df = pd.DataFrame(data=data)
+        df.to_csv(f"exp_data/csv_data_cond{i}", index=False)
 
 
 def get_all_attacked_docs(attack_path):
@@ -35,13 +50,12 @@ def get_all_attacked_docs(attack_path):
         print(attack)
     
 
-
-
-
 if __name__ == "__main__":
     ground_truth = "../bayesian_shielding/benchmark_tasks/STSB/400_sentences.csv"
     attack_path = "attacked_documents"
-    attacked_documents = ["phonetic_10.txt", "phonetic_50.txt", "phonetic_70.txt", "phonetic_90.txt", "rand1.txt"]
-    sentence_per_doc = [20, 20, 20, 20, 40]
+    attacked_documents = ["rand2.txt", "phonetic.txt", "phonetic_70.txt", "visual.txt",
+                          "full_swap.txt", "natural_typo.txt", "segment_typo.txt",
+                          "disemvowel.txt", "rand_hard.txt"]
+    sentence_per_attacker = 40
     get_all_attacked_docs(attack_path)
-    create_human_test_data(ground_truth, attack_path, attacked_documents, sentence_per_doc, 3)
+    create_human_test_data(ground_truth, attack_path, attacked_documents, sentence_per_attacker, 4, 27)
